b7cc1b46fb096b0069673bb002917f49a4afc474
Leandro Soria <leandrosoria@hotmail.com>
2024-04-09 00:05:31 -0300

    Actualizados comentarios de TB de aes_shift_rows

--- tp-final/src/tb/aes_shift_rows_tb.vhd
+++ tp-final/src/tb/aes_shift_rows_tb.vhd
@@ -32,11 +32,19 @@ begin
 
     stimulus_process: process
     begin
-        tb_state_in <= x"000102030405060708090a0b0c0d0e0f"; -- expected output: x"00 05 0a 0f 04 09 0e 03 08 0d 02 07 0c 01 06 0b"
+        -- expected output (normal):   x"00 05 0a 0f 04 09 0e 03 08 0d 02 07 0c 01 06 0b"
+        -- expected output (inverted): x"00 0d 0a 07 04 01 0e 0b 08 05 02 0f 0c 09 06 03"
+        tb_state_in <= x"000102030405060708090a0b0c0d0e0f";
         wait for 100 ns;
-        tb_state_in <= x"00112233445566778899aabbccddeeff"; -- expected output: x"00 55 aa ff 44 99 ee 33 88 dd 22 77 cc 11 66 bb"
+
+         -- expected output (normal):   x"00 55 aa ff 44 99 ee 33 88 dd 22 77 cc 11 66 bb"
+         -- expected output (inverted): x"00 dd aa 77 44 11 ee bb 88 55 22 ff cc 99 66 33"
+        tb_state_in <= x"00112233445566778899aabbccddeeff";
         wait for 100 ns;
-        tb_state_in <= x"ffeeddccbbaa99887766554433221100"; -- expected output: x"ff aa 55 00 bb 66 11 cc 77 22 dd 88 33 ee 99 44"
+
+         -- expected output (normal):   x"ff aa 55 00 bb 66 11 cc 77 22 dd 88 33 ee 99 44"
+         -- expected output (inverted): x"ff 22 55 88 bb ee 11 44 77 aa dd 00 33 66 99 cc"
+        tb_state_in <= x"ffeeddccbbaa99887766554433221100";
         wait for 100 ns;
         wait; -- Wait indefinitely
     end process stimulus_process;
